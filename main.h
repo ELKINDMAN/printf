1b04f890d29337fd1c14d72684fa9191fccda3d7
elkanah <elkanahkindness@gmail.com>
2023-11-10 04:02:13 -0800

    basic files

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
